30b6a7624ad30d4c72e3ae595a4ebe8ccc902727
Julio González Gil <jgonzalez@suse.com>
2017-09-28 19:43:10 +0200

    Make clone operations simplier

--- lib/gitarro/git_op.rb
+++ lib/gitarro/git_op.rb
@@ -1,131 +1,106 @@
 #! /usr/bin/ruby
 
-require 'English'
 require 'fileutils'
-require 'timeout'
+require 'English'
 
-# This class is used by lib/backend.rb
-# git operation for gitarro
+# Representation of a commit with its attributes
+class Commit
+  attr_accessor :hash, :author_name, :author_email, :date
+end
+
+# git operation for gitbot
 class GitOp
-  attr_reader :git_dir, :pr, :pr_fix, :repo_external, :repo_protocol
-  def initialize(git_dir, pr, options)
-    @git_dir = git_dir
-    # prefix for the test pr that gitarro tests.
-    @pr_fix = 'PR-'
-    # pr object for extract all relev. data.
-    @pr = pr
-    # All gitarro options
-    @options = options
-    # object to handle external repos
-    @repo_external = ExternalRepoGit.new(pr, options)
-    gh = 'https://github.com/'
-    gg = 'git@github.com:'
-    @repo_protocol = @options[:https] ? gh : gg
+  def initialize(git_dir, options)
+    # GitHub URL depending on protocol
+    @repo_url = options[:https] ? 'https://github.com/' : 'git@github.com:'
+    @repo_url = @repo_url + options[:repo] + '.git'
+    # Repository name (without project)
+    repo_name = options[:repo].split('/')[1]
+    # Directory where the repository with the code to test will live
+    # append repository name to be retrocompatible
+    # But if it exists, use /tmp/gitbot/<repo_name>
+    @git_dir = git_dir + '/' + repo_name
+    # Clone style
+    @no_merge_upstream = options[:no_merge_upstream]
   end
 
-  # merge pr_branch into upstream targeted branch
-  def merge_pr_totarget(upstream, pr_branch)
-    goto_prj_dir
-    check_git_dir
-    `git checkout #{upstream}`
-    check_duplicata_pr_branch("#{pr_fix}#{pr_branch}")
-    `git remote update`
-    `git fetch`
-    `git pull origin #{upstream}`
-    `git checkout -b #{pr_fix}#{pr_branch} origin/#{pr_branch}`
-    return if $CHILD_STATUS.exitstatus.zero?
-    # if it fails the PR contain a forked external repo
-    repo_external.checkout_into
-  end
-
-  # cleanup the pr_branch(delete it)
-  def del_pr_branch(upstream, pr)
-    `git checkout #{upstream}`
-    `git branch -D  #{pr_fix}#{pr}`
-  end
-
-  private
-
-  def ck_or_clone_git
-    return if File.directory?(git_dir)
-    FileUtils.mkdir_p(git_dir)
-    Dir.chdir git_dir
-    repo_url = "#{repo_protocol}#{@options[:repo]}.git"
-    puts `git clone #{repo_url}`
-  end
-
-  # this function merge the pr branch  into target branch,
-  # where the author of pr wanted to submit
-  def goto_prj_dir
-    git_repo_dir = git_dir + '/' + @options[:repo].split('/')[1]
-    # chech that dir exist, otherwise clone it
-    ck_or_clone_git
-    begin
-      # /tmp/gitarro, this is in case the dir already exists
-      Dir.chdir git_repo_dir
-    rescue Errno::ENOENT
-      # this is in case we clone the repo
-      Dir.chdir @options[:repo].split('/')[1]
+  def get_pr(pr_number, pr_base_ref)
+    git_init(@git_dir)
+    git_remote_add('origin', @repo_url)
+    # PR standalone, last commit
+    if @no_merge_upstream
+      git_pull('origin', "refs/pull/#{pr_number}/head")
+    # The PR plus all commits from base until the common ancestor
+    else
+      # Rewind until we can pull --rebase pr_base_ref
+      git_pull_rewind('origin', "refs/pull/#{pr_number}/head", pr_base_ref)
     end
   end
 
-  def check_git_dir
-    msg_err = 'gitarro is not working on a git directory'
-    raise msg_err if File.directory?('.git') == false
+  def can_pull_merge(origin, base_ref, depth)
+    puts `git pull --depth #{depth} --no-edit #{origin} #{base_ref}`
+    $CHILD_STATUS.exitstatus.zero?
   end
 
-  # this is for preventing that a test branch exists already
-  # and we have some internal error
-  def check_duplicata_pr_branch(pr)
-    puts `git branch --list #{pr}`
-    `git branch -D #{pr} 2>/dev/null` if $CHILD_STATUS.exitstatus.zero?
-  end
-end
-
-# This private class handle the case the repo from PR
-# comes from a user external repo
-# PR open against: openSUSE/gitarro
-# PR repo:  MyUSER/gitarro
-class ExternalRepoGit
-  attr_reader :pr, :rem_repo, :pr_fix
-  def initialize(pr, options)
-    # pr object for extract all relev. data.
-    @pr = pr
-    @pr_fix = 'PR-'
-    @options = options
+  def error_max_rewind(cur, max, ref)
+    return unless cur >= max
+    puts "\nERROR: Rewinded more than commits 100 without being able to merge!"
+    puts "And cound not find it!\n\n"
+    puts "Either PR lacks of a parent commit at #{ref} you just have too"
+    puts 'many commits and you need to squash your commits to make the PR '
+    puts "easier!\n"
+    exit 1
   end
 
-  def checkout_into
-    rem_repo = 'rem' + pr.head.ref
-    add_remote(rem_repo)
-    fetch_remote(rem_repo)
-    checkout_to_rem_branch(rem_repo)
-    remove_repo(rem_repo)
+  def git_init(dir)
+    # Remove the directory if it exists
+    FileUtils.remove_entry_secure(dir) if File.directory?(dir)
+    puts `git init #{dir}`
+    exit 1 if $CHILD_STATUS.exitstatus.nonzero?
+    Dir.chdir dir
   end
 
-  private
-
-  def checkout_to_rem_branch(rem_repo)
-    puts `git checkout -b #{pr_fix}#{branch_rem} #{rem_repo}/#{branch_rem}`
+  def git_remote_add(remote, url)
+    puts `git remote add #{remote} #{url}`
     exit 1 if $CHILD_STATUS.exitstatus.nonzero?
   end
 
-  def branch_rem
-    pr.head.ref
+  def git_pull_rewind(origin, ref, base_ref)
+    # Download commits backwards starting from PR head until we can rebase
+    # base_ref
+    i = 1
+    loop do
+      puts "\n*** Downloading from #{origin} #{ref}, depth #{i}\n\n"
+      git_pull(origin, ref, i)
+      puts "\n*** Downloading from #{origin} #{base_ref}, depth #{i}\n\n"
+      break if can_pull_merge(origin, base_ref, i)
+      i += 1
+      error_max_rewind(i, 101, base_ref)
+    end
   end
 
-  def add_remote(rem_repo)
-    repo_url = @options[:https] ? pr.head.repo.html_url : pr.head.repo.ssh_url
-    puts `git remote add #{rem_repo} #{repo_url}`
+  def git_pull(remote, ref, depth = 1)
+    # To support changing pulling depth in the future
+    depth_param = depth == -1 ? '' : "--depth #{depth}"
+    puts `git pull #{depth_param} #{remote} #{ref}`
+    exit 1 if $CHILD_STATUS.exitstatus.nonzero?
   end
 
-  def fetch_remote(rem_repo)
-    puts `git remote update`
-    puts `git fetch`
-    puts `git pull #{rem_repo} #{pr.head.ref}`
+  def current_commit
+    commit = Commit.new
+    commit.hash = `git log --pretty=format:'%H' -n 1`
+    commit.author_name = `git log --pretty=format:'%an' -n 1`
+    commit.author_email = `git log --pretty=format:'%ae' -n 1`
+    commit.date = `git log --pretty=format:'%ad' -n 1`
+    commit
   end
 
-  def remove_repo(rem_repo)
-    puts `git remote remove #{rem_repo}`
+  def print_commit_info(pr)
+    commit = current_commit
+    puts "\nThe following commit is to be tested:\n" \
+      "- LOCAL HASH:  #{commit.hash}\n" \
+      "- REPO ORIG:   #{pr.head.repo.full_name}\n" \
+      "- BRANCH_ORIG: #{pr.head.ref}\n" \
+      "- HASH_ORIG:   #{pr.head.sha}\n\n" \
   end
 end
