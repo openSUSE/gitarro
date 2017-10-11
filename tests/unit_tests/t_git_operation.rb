bd81bb37fd2b3b8b5a64af5e70c200d1a9dfe2e7
MalloZup <dmaiocchi@suse.com>
2017-10-11 10:39:39 +0200

    Add unit_test for gitop
    Fix #76

--- tests/unit_tests/t_git_operation.rb
+++ tests/unit_tests/t_git_operation.rb
@@ -15,4 +15,18 @@ class GitarroGitop < Minitest::Test
     GitOp.new(gb.git_dir, pr, @full_hash)
     puts gb.git_dir
   end
+
+  def test_gitop_dirnonexist
+    @full_hash = { repo: 'openSUSE/gitarro', context: 'python-t', description:
+                   'functional', test_file: 'gino.sh', file_type: '.sh',
+                   git_dir: '/tmp/gitarro_test',
+                   https: true, changed_since: -1 }
+    gb = Backend.new(@full_hash)
+    # crate fake object for internal class external repo
+    # FIXME: this could improved creating a full mock obj
+    pr = 'fake'
+    git = GitOp.new(gb.git_dir, pr, @full_hash)
+    git.merge_pr_totarget('master', 'master')
+    assert(true, File.directory?(gb.git_dir))
+  end
 end
